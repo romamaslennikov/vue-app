00a6006cf6087b06c323fd4d2ee9359e81e30367
Рома Масленников <maslennikov.rv@gmail.com>
2024-08-31 08:09:01 +0400

    fixation

--- src/components/ui/Popover.vue
+++ src/components/ui/Popover.vue
@@ -1,16 +1,24 @@
 /** * @usage: popover template(v-slot:action) .icon template(v-slot:body) div Текст * */
 
 <template lang="pug">
-  .popover
-    slot(name="action")
+  .popover(
+    :class="skin"
+    v-click-outside="() => (isShow = false)")
+    .popover__action(@click="toggle")
+      slot(name="action")
 
-    .popover__body(v-if="!disabled")
-      slot(name="body")
+    transition(name="fade")
+      .popover__body(
+        ref="popoverBody"
+        v-if="isShow")
+        slot(name="body")
+
+        .popover__close.icon.icon__x2(@click="toggle")
 </template>
 
 <script>
 import { typograf } from '@/utils/format';
-import { computed } from 'vue';
+import { computed, nextTick, ref } from 'vue';
 import { useAppStore } from '@/stores/app';
 
 export default {
@@ -21,22 +29,53 @@ export default {
       type: Boolean,
       default: false,
     },
+
+    skin: {
+      type: String,
+      default: '',
+    },
   },
 
   setup() {
     // data
     const appStore = useAppStore();
+    const isShow = ref(false);
+    const popoverBody = ref(null);
 
     // computed
     const isPortrait = computed(() => appStore.isPortrait);
 
     // methods
+    function resolvePosition() {
+      const target = popoverBody.value;
+
+      if (target) {
+        const { clientWidth } = document.documentElement;
+        const { right, left } = target.getBoundingClientRect();
+        const deltaRight = clientWidth - right;
+
+        if (deltaRight < 0) {
+          target.style.marginLeft = `${deltaRight}px`;
+        } else if (left < 0) {
+          target.style.marginLeft = `${Math.abs(left)}px`;
+        }
+      }
+    }
+
+    function toggle() {
+      isShow.value = !isShow.value;
+
+      nextTick(resolvePosition);
+    }
 
     // hooks
 
     return {
+      popoverBody,
+      isShow,
       isPortrait,
       typograf,
+      toggle,
     };
   },
 
@@ -44,51 +83,49 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-.popover {
-  position: relative;
-  display: inline-flex;
+<style scoped lang="sass">
+.popover
+  display: inline
+  position: relative
 
-  &__body {
-    position: absolute;
-    bottom: 100%;
-    left: 50%;
-    z-index: 1000;
-    padding: rem(18px);
-    margin-bottom: rem(12px);
-    width: rem(272px);
-    font-size: rem(12px);
-    color: #333;
-    background: $color-white;
-    border-radius: rem(10px);
-    opacity: 0;
-    visibility: hidden;
-    box-shadow: 0 0.5rem 0.5rem rgba(0, 0, 0, 0.2);
-    transition: all $transition ease-out;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    transform: translate(-50%, -0.5rem);
+  &__action
+    cursor: pointer
+    display: inline
 
-    &::before {
-      content: '';
-      position: absolute;
-      bottom: rem(-7px);
-      left: 50%;
-      width: rem(14px);
-      height: rem(14px);
-      background: $color-white;
-      border-radius: rem(2px);
-      transform: translate(-50%) rotate(-45deg);
-    }
-  }
+  &__close
+    position: absolute
+    top: rem(17px)
+    right: rem(17px)
+    font-size: rem(14px)
+    line-height: 1
+    color: #D1D0D0
+    cursor: pointer
+    &:hover
+      color: $color-red
+    +media($port)
+      font-size: rem(12px)
+      top: rem(14px)
+      right: rem(14px)
 
-  &:hover {
-    .popover__body {
-      opacity: 1;
-      visibility: visible;
-      transform: translate(-50%, 0);
-    }
-  }
-}
+  &__body
+    box-shadow: 0 0.5rem 0.5rem rgba(0, 0, 0, .2)
+    position: absolute
+    bottom: 100%
+    left: 50%
+    width: rem(352px)
+    z-index: 1000
+    background: $color-black url("@images/quod/popover-bg.png") no-repeat 50% 50% / 100% 100%
+    font-family: $family-Maestro
+    padding: rem(40px)
+    color: #D1D0D0
+    text-transform: uppercase
+    font-size: rem(18px)
+    line-height: 120%
+    font-style: normal
+    font-weight: 400
+    transform: translate(-50%, -.5rem)
+    +media($port)
+      width: rem(273px)
+      font-size: rem(13px)
+      padding: rem(35px)
 </style>
